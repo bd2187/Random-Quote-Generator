36d2501957318238e21512f1b8971857eb5a8f66
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-05-05 22:41:30 -0700

    Add generateQuote(), adjustHTML(), and randomNumberGenerator()

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-console.log(quotes.length);
+generateQuote();
+function generateQuote() {
+  var num       = randomNumberGenerator(quotes);
+  var quote     = quotes[num].quote; // bug present in one of the quotes
+  var character = quotes[num].character;
+  var film      = quotes[num].film;
+  return adjustHTML(quote, character, film);
+}
+
+function adjustHTML(quote, character, film) {
+  var quoteEl       = document.querySelector('.quote');
+  var characterEl   = document.querySelector('.character');
+  var filmEl        = document.querySelector('.film');
+
+  quoteEl.textContent = quote;
+  characterEl.textContent = character;
+  filmEl.textContent = film;
+}
+
+function randomNumberGenerator(arr = []) {
+  var randomNum = Math.floor(Math.random() * arr.length);
+  return randomNum;
+}
