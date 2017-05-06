48de1ffe900ea0f238744ea3f829d7edbcccdb2d
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-05-06 16:44:58 -0700

    Add tweet button index.html. Add tweet function in module along with twitterBtn. Transferred querySelectors from functional scope of adjustHTML() to outer scope.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 var mod = (function() {
-  var newQuoteBtn = document.querySelector('.newQuote');
+  var quoteEl       = document.querySelector('.quote');
+  var characterEl   = document.querySelector('.character');
+  var filmEl        = document.querySelector('.film');
+  var newQuoteBtn   = document.querySelector('.newQuote');
+  var twitterBtn    = document.querySelector('.twitter');
 
   function generateQuote() {
     var num       = randomNumberGenerator(quotes); // found in quotes.js
@@ -9,30 +13,35 @@ var mod = (function() {
     return adjustHTML(quote, character, film);
   }
 
-  function adjustHTML(quote, character, film) {
-    var quoteEl       = document.querySelector('.quote');
-    var characterEl   = document.querySelector('.character');
-    var filmEl        = document.querySelector('.film');
+  function randomNumberGenerator(arr = []) {
+    var randomNum = Math.floor(Math.random() * arr.length);
+    return randomNum;
+  }
 
+  function adjustHTML(quote, character, film) {
     quoteEl.textContent = quote;
     characterEl.textContent = character;
     filmEl.textContent = film;
   }
 
-  function randomNumberGenerator(arr = []) {
-    var randomNum = Math.floor(Math.random() * arr.length);
-    return randomNum;
+  function tweet() {
+    return twitterBtn.setAttribute('href', `https://twitter.com/intent/tweet?text=${quoteEl.textContent} - ${characterEl.textContent}`);
   }
 
   return {
     foo: function() {
       return generateQuote();
     },
-    publicEventListener() {
+    publicQuoteGenerator() {
       return newQuoteBtn.addEventListener('click', generateQuote);
+    },
+    publicTweetGenerator() {
+      twitterBtn.addEventListener('click', tweet);
     }
+
   }
 })();
 
 window.addEventListener('load', mod.foo);
-mod.publicEventListener();
+mod.publicQuoteGenerator();
+mod.publicTweetGenerator();
