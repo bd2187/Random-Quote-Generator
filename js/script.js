5fc382e33818daf00d5fa6af09361be18438e717
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-05-06 15:20:10 -0700

    Add module pattern to protect variables and functions from global scope

--- js/script.js
+++ js/script.js
@@ -1,26 +1,34 @@
-var newQuoteBtn = document.querySelector('.newQuote');
-newQuoteBtn.addEventListener('click', generateQuote);
+var mod = (function() {
+  var newQuoteBtn = document.querySelector('.newQuote');
 
+  function generateQuote() {
+    var num       = randomNumberGenerator(quotes); // found in quotes.js
+    var quote     = quotes[num].quote; // bug present in one of the quotes
+    var character = quotes[num].character;
+    var film      = quotes[num].film;
+    return adjustHTML(quote, character, film);
+  }
 
-function generateQuote() {
-  var num       = randomNumberGenerator(quotes);
-  var quote     = quotes[num].quote; // bug present in one of the quotes
-  var character = quotes[num].character;
-  var film      = quotes[num].film;
-  return adjustHTML(quote, character, film);
-}
+  function adjustHTML(quote, character, film) {
+    var quoteEl       = document.querySelector('.quote');
+    var characterEl   = document.querySelector('.character');
+    var filmEl        = document.querySelector('.film');
 
-function adjustHTML(quote, character, film) {
-  var quoteEl       = document.querySelector('.quote');
-  var characterEl   = document.querySelector('.character');
-  var filmEl        = document.querySelector('.film');
+    quoteEl.textContent = quote;
+    characterEl.textContent = character;
+    filmEl.textContent = film;
+  }
 
-  quoteEl.textContent = quote;
-  characterEl.textContent = character;
-  filmEl.textContent = film;
-}
+  function randomNumberGenerator(arr = []) {
+    var randomNum = Math.floor(Math.random() * arr.length);
+    return randomNum;
+  }
 
-function randomNumberGenerator(arr = []) {
-  var randomNum = Math.floor(Math.random() * arr.length);
-  return randomNum;
-}
+  return {
+    publicEventListener() {
+      return newQuoteBtn.addEventListener('click', generateQuote);
+    }
+  }
+})();
+
+mod.publicEventListener();
